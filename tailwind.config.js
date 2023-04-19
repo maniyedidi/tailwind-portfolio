b90b97abd05d2d20b78249623cf0a2d0423fe9ff
maniyedidi <yedidi-satya.manikanta@sixt.com>
2023-04-19 21:16:21 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },    
+    extend: {
+      colors: {
+        primary: '#198754',
+        primaryLight: '#21C896'
+      }
+    }
+  },
+  plugins: []
+};
