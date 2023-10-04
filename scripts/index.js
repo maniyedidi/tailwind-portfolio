0f14fcad82f66e55a95295bf8841dc421ba2766e
maniyedidi <yedidi-satya.manikanta@sixt.com>
2023-10-04 09:45:17 +0530

    added new projects

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,71 @@
 function openMenu() {
-  const e = document.getElementById('menu-btn');
-  e.classList.toggle('open');
+  const e = document.getElementById("menu-btn");
+  e.classList.toggle("open");
 
-  const menu = document.getElementById('menu');
-  menu.classList.toggle('flex');
-  menu.classList.toggle('hidden');
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function plusSlides2(n) {
+  showSlides2((slideIndex2 += n));
+}
+
+function currentSlide2(n) {
+  showSlides2((slideIndex2 = n));
+}
+
+function showSlides2(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides2");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex2 = 1;
+  }
+  if (n < 1) {
+    slideIndex2 = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex2 - 1].style.display = "block";
+  dots[slideIndex2 - 1].className += " active";
 }
